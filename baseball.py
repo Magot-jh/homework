f63dbf58aee4de2a8172d33ab4d71cb5918429f2
Magot-jh <67869438+Magot-jh@users.noreply.github.com>
2020-07-25 17:54:38 +0900

    Create baseball.py

---
+++ baseball.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+# URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://sports.news.naver.com/kbaseball/record/index.nhn?category=kbo',headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+soup = BeautifulSoup(data.text, 'html.parser')
+
+baseball = soup.select('#regularTeamRecordList_table > tr')
+
+for base in baseball:
+    rank = base.select_one('th > strong')
+    print(rank.text)
